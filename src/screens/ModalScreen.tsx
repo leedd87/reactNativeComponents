6bbe7df882d63f1bb03b7184be2024c010e2d2a2
leedd87 <leedav87@gmail.com>
2023-01-06 09:55:22 -0300

    add modal component

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import { View } from 'react-native';
+import React, { useState } from 'react'
+import { Button, Modal, Text, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
 export const ModalScreen = () => {
+
+    const [isVisible, setIsVisible] = useState(false)
+
     return (
         <View style={styles.globalMargin}>
             <HeaderTitle title='ModalScreen' />
+
+
+
+            <Button title='Abrir modal'
+                onPress={() => setIsVisible(true)}
+            />
+
+            <Modal
+                animationType='fade'
+                visible={isVisible}
+                transparent={true}
+            >
+                <View style={{
+                    flex: 1,
+                    backgroundColor: 'rgba(0,0,0,0.3)',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <View style={{
+                        width: 200,
+                        height: 200,
+                        backgroundColor: 'white',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 5,
+                        shadowOffset: {
+                            width: 0,
+                            height: 10
+                        },
+                        shadowOpacity: 0.25,
+                        elevation: 10,
+
+                    }}>
+
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
+                        <Text style={{ fontSize: 16, fontWeight: "300", marginBottom: 20 }}>Cuerpo del modal</Text>
+                        <Button
+                            title='Cerrar'
+                            onPress={() => setIsVisible(false)}
+                        />
+                    </View>
+
+                </View>
+
+            </Modal>
         </View>
     )
 }

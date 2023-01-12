afa1339cb71ffdede5a4548fa86ecace7f6a4f26
leedd87 <leedav87@gmail.com>
2023-01-12 09:56:44 -0300

    add theme reducer

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,18 +1,20 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
+import { lightTheme, themeReducer, ThemeState } from './themeReducer';
 
 interface ThemeContextProps {
-    theme: any; //TODO:
+    theme: ThemeState //TODO:
     setDarkTheme: () => void;
     setLightTheme: () => void;
 }
 
 export const ThemeContext = createContext({} as ThemeContextProps)
 
-const theme = {};
 
 export const ThemeProvider = ({ children }: any) => {
+    const [theme, dispatch] = useReducer(themeReducer, lightTheme) //TODO: leer el tema global
 
     const setDarkTheme = () => {
+        dispatch({ type: 'set_dark_theme' })
         console.log('setDarkTheme')
     }
 
---
+++ src/context/themeContext/themeReducer.tsx
@@ -0,0 +1,37 @@
+import { Theme } from "@react-navigation/native"
+
+type ThemeAction =
+    | { type: 'set_light_theme' }
+    | { type: 'set_dark_theme' }
+
+export interface ThemeState extends Theme {
+    currentTheme: 'light' | 'dark',
+    dividerColor: string
+}
+
+export const lightTheme: ThemeState = {
+    currentTheme: 'light',
+    dark: false,
+    dividerColor: 'green',
+    colors: {
+        primary: 'red',
+        background: 'blue',
+        card: 'green',
+        text: 'pink',
+        border: 'orange',
+        notification: 'teal'
+    }
+}
+
+export const themeReducer = (state: ThemeState, action: ThemeAction): ThemeState => {
+
+    switch (action.type) {
+        case 'set_light_theme':
+            return { ...lightTheme }
+
+        default:
+            return state;
+    }
+
+
+}
